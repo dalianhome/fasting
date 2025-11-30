53b80742b6b27bec690ce4316b21d401b786b040
samlee20051985@gmail.com <19851018Eva@>
2025-11-30 10:02:26 +0800

    Initial Commit

---
+++ fastingBuddy/fastingBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastingBuddyApp.swift
+//  fastingBuddy
+//
+//  Created by chengxin on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct fastingBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
